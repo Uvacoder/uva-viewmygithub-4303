2e8ab180e18694378595f6a1180c3a5711a1fdd1
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-04 18:33:11 -0500

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,10 +2,10 @@
   <div class="p-4 bg-gray-800 w-full text-gray-50 flex justify-center">
     <div class="w-8/12 flex justify-center">
       <a
-        href="https://mehmetali345.xyz"
+        href="https://abclinks.vercel.app"
         title="Developer"
         class="hover:(transition ease-in-out duration-300 text-red-600) text-lg text-center md:text-xl font-bold"
-      >Made by Mehmetali345Dev with Love</a>
+      >Made by uvacoder</a>
     </div>
   </div>
 </template>
