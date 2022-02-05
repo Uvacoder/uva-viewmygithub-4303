7c38d35878c8ad71b00525b22a8116974a006bca
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-02-04 19:37:08 -0500

    Update Nav.vue

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,10 +2,10 @@
   <div class="p-4 bg-gray-800 w-full text-gray-50 flex justify-center">
     <div class="w-8/12 flex items-center space-x-3 flex-wrap justify-center">
       <a
-        href="https://github.com/mehmetali345dev/github-profile-viewer"
+        href="https://github.com/uvacoder/githubuser-view"
         title="Developer"
         class="hover:(transition ease-in-out duration-300 text-red-600) md:text-xl text-lg font-bold"
-      >Github Profile Viewer</a>
+      >View GithubUser Profile</a>
       <nuxt-link to="/" class="bg-gray-700 p-2 rounded-md font-bold" v-if="$route.params.user">Go back</nuxt-link>
     </div>
   </div>
