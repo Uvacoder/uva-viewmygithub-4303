795dca73632d50829e49566332b9e2d64e36f040
uvacoder <uva22940coder@gmail.com>
2023-01-28 01:55:59 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,16 @@
     <div class="w-full h-full flex justify-center items-center">
       <div class="gap-4 grid m-4">
         <div class="justify-center flex flex-col space-y-3 items-start">
-          <h1 class="text-gray-50 font-bold text-xl">Search github username</h1>
+          <h1 class="text-gray-50 font-bold text-3xl">Search github username</h1>
           <input
             v-model="user"
             placeholder="Search an user"
             type="text"
-            class="bg-gray-800 p-4 w-full text-gray-50 rounded-md"
+            class="bg-gray-800 p-4 w-full text-gray-50 rounded-md w-80"
           />
           <nuxt-link
             :to="{ name: 'u-user', params: { user: user } }"
-            class="bg-red-600 flex justify-center p-4 rounded-md font-bold text-gray-50 w-full"
+            class="bg-red-600 flex justify-center p-4 rounded-md font-bold text-gray-50 w-full w-80"
           >Go!</nuxt-link>
         </div>
       </div>
